383ccad3060f266b8f196221f804804122c4843d
Marat Khabibullin <khabibullin.m.m@gmail.com>
2023-07-09 09:15:05 +0300

    chat ui

--- chat_ui/src/App.tsx
+++ chat_ui/src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import './App.css';
+import ChatUI from './components/ChatUI';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl" sx={{ p: '0px !important' }} >
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              Skills Assessment Chat
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box
+        component="form"
+        sx={{
+          mt: 3,
+          '& .MuiTextField-root': { my: 1, width: '100%' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Container maxWidth="xl">
+          <ChatUI></ChatUI>
+        </Container>
+      </Box>
+    </Container>
   );
 }
 
